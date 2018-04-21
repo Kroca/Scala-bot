8470a3bc9f0be919f2958f04b5562fa387734b13
Kroca <khoazat1@gmail.com>
2018-04-21 14:25:21 +0300

    added stats about stock /sv "name"

--- src/main/scala/MyStockBot.scala
+++ src/main/scala/MyStockBot.scala
@@ -6,30 +6,17 @@ import info.mukel.telegrambot4s.Implicits._
 import info.mukel.telegrambot4s.api._
 import info.mukel.telegrambot4s.api.declarative._
 import info.mukel.telegrambot4s.methods.ParseMode
+import model.{DataISIN, DataStock}
+import java.util.Date
+import java.time.LocalDate
+
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
 import scala.collection.mutable.ListBuffer
 
-
 object MyStockBot extends TelegramBot with Polling with InlineQueries with Commands {
   implicit val formats = DefaultFormats
-  case class Data(id:String,
-                  secid:String,
-                  shortname:String,
-                  regnumber:String,
-                  name:String,
-                  isin:String,
-                  is_traded:String,
-                  emitend_id:String,
-                  emitent_title:String,
-                  emitent_inn:String,
-                  emitent_okpo:String,
-                  gosreg:String,
-                  _type:String,
-                  group:String,
-                  primary_boardid:String,
-                  marketprice_boardid:String)
 
   override def token: String = "564399150:AAESOLtP7aKK3G9BphDCwRtXHh-4es1x7Kc"
 
@@ -46,11 +33,53 @@ object MyStockBot extends TelegramBot with Polling with InlineQueries with Comma
       parseMode = ParseMode.Markdown)
   }
 
+  onCommand('sv) { implicit msg =>
+    withArgs { args =>
+      val query = args.mkString(" ")
 
+      val url = Uri("https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/"+query+".json")
+        .withQuery(Query("from" -> LocalDate.now.minusDays(1).toString))
+        .toString()
+      print(url)
+      for {
+        response <- Http().singleRequest(HttpRequest(uri = Uri(url)))
+        if response.status.isSuccess()
+        json <- Unmarshal(response).to[String]
+      } /* do */ {
+        print(json)
+        val objs = parse(json)
+        val input = objs \ "history" \ "data"
+        val data = input.extract[List[List[String]]]
+
+        var storage = new ListBuffer[DataStock]()
+        for(d <-data){
+          storage+= DataStock(d(0),d(1),d(2),d(3),d(4),d(5),d(6),d(7),d(8),d(9),d(10),d(11),d(12),d(13),d(14),d(15),d(16),d(17),d(18),d(19))
+        }
+        var result = ""
+        for(s <- storage){
+          if(s.boardid == "TQBR"){
+            val dif = ((s.marketprice2.toFloat/s.open.toFloat - 1)*100)
+            result += s.secid + " - " + s.shortname + "\n \n" +
+              "Открытие: "+ s.open + "\n" +
+              "Текущая цена: " + s.marketprice2 + " \n" +
+              "Изменение: "+f"$dif%1.2f"+ "\n"
+          }
+        }
+        if(result==""){
+          reply("Bad request, you probably mistyped secid")
+        }else{
+          reply(result)
+        }
+
+      }
+    }
+  }
   onCommand('search) { implicit msg =>
     withArgs { args =>
       val query = args.mkString(" ")
-      val url = moexUrl(query)
+      val url = Uri("https://iss.moex.com/iss/securities.json")
+        .withQuery(Query("q" -> query))
+        .toString()
       for {
         response <- Http().singleRequest(HttpRequest(uri = Uri(url)))
         if response.status.isSuccess()
@@ -61,9 +90,9 @@ object MyStockBot extends TelegramBot with Polling with InlineQueries with Comma
         val input = objs \ "securities" \ "data"
         val data = input.extract[List[List[String]]]
 
-        var storage = new ListBuffer[Data]()
+        var storage = new ListBuffer[DataISIN]()
         for(d <-data){
-          storage+= Data(d(0),d(1),d(2),d(3),d(4),d(5),d(6),d(7),d(8),d(9),d(10),d(11),d(12),d(13),d(14),d(15))
+          storage+= DataISIN(d(0),d(1),d(2),d(3),d(4),d(5),d(6),d(7),d(8),d(9),d(10),d(11),d(12),d(13),d(14),d(15))
         }
         var result = ""
         for(s <- storage){
@@ -76,8 +105,4 @@ object MyStockBot extends TelegramBot with Polling with InlineQueries with Comma
     }
   }
 
-  def moexUrl(query: String): String =
-    Uri("https://iss.moex.com/iss/securities.json")
-      .withQuery(Query("q" -> query))
-      .toString()
 }
