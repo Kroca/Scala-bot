338bf002391c148b4c70cf40918ab937cb57d91a
Mergaliev Ibragim <mergalievibragim@gmail.com>
2018-04-27 19:18:29 +0300

    Change add and delete methods

--- src/main/scala/AuthenticationBot.scala
+++ src/main/scala/AuthenticationBot.scala
@@ -12,20 +12,4 @@ class AuthenticationBot() extends TelegramBot with Polling with Commands with Si
   override def token: String = "598435632:AAEkD_zpBTtOFLZtoN7lMM9uP3umArhZvnY"
 
 
-  onCommand("/login") { implicit msg =>
-    for (user <- msg.from) {
-      login(user)
-      if (MyStockBot.important_shares.get(user.id).isEmpty) {
-        MyStockBot.important_shares(user.id) = scala.collection.mutable.Set()
-      }
-    }
-    reply("Now you have access")
-  }
-
-  onCommand("/logout") { implicit msg =>
-    for (user <- msg.from)
-      logout(user)
-    reply("Bye bye!")
-  }
-
 }
