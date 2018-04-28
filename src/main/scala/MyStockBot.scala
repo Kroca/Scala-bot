32868646739dfb46e09da573898fc7a8ed7fc286
Kroca <khoazat1@gmail.com>
2018-04-28 21:42:07 +0300

    command /info update

--- src/main/scala/MyStockBot.scala
+++ src/main/scala/MyStockBot.scala
@@ -1,3 +1,4 @@
+import java.awt.Color
 import java.nio.file.{Files, Paths}
 import java.text.SimpleDateFormat
 
@@ -17,7 +18,7 @@ import scalax.chart.api._
 import akka.util.ByteString
 import info.mukel.telegrambot4s.models.InputFile
 import org.jfree.chart.axis.DateAxis
-import org.jfree.data.time.TimeSeriesCollection
+import org.jfree.data.time.{RegularTimePeriod, Second, TimeSeriesCollection}
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
@@ -43,27 +44,28 @@ object MyStockBot extends AuthenticationBot with Polling with InlineQueries with
     reply(
       s"""Moscow stock monitoring bot made by ALPHA TEAM.
          |
-         |/start | /help - list commands
+         |/start - list commands
          |
-         |/search args - provides info
+         |/search %name% - provides info about stocks including id for further searches
+         |/info %id% - shows stock info for specific company provides actual info for today and graph of changes for the week
+         |/list - shows the list of important shares
+         |/add - adds a share to the list of important shares
+         |/delete -  adds a share to the list of important shares
          |
-         |/list - Only authenticated users have access, shows the list of important shares
-         |/add - Only authenticated users have access, adds a share to the list of important shares
-         |/delete - Only authenticated users have access, adds a share to the list of important shares
-         |
-         |@Bot args - Inline mode
+         |/Stock market doesn't work on holidays!.
+
       """.stripMargin,
       parseMode = ParseMode.Markdown)
   }
-
-  onCommand('sv) { implicit msg =>
+  //no info available at holidays
+  onCommand('info) { implicit msg =>
     withArgs { args =>
       val query = args.mkString(" ")
 
       val url = Uri("https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/"+query+".json")
-        .withQuery(Query("from" -> LocalDate.now.minusDays(1).toString))
+        .withQuery(Query("from" -> LocalDate.now.toString))
         .toString()
-      print(url)
+      createGraph(query)
       for {
         response <- Http().singleRequest(HttpRequest(uri = Uri(url)))
         if response.status.isSuccess()
@@ -81,7 +83,7 @@ object MyStockBot extends AuthenticationBot with Polling with InlineQueries with
         var result = ""
         for(s <- storage){
           if(s.boardid == "TQBR"){
-            val dif = ((s.marketprice2.toFloat/s.open.toFloat - 1)*100)
+            val dif = ((s.marketprice2.toFloat/s.open.toFloat -1 )*100)
             result += s.secid + " - " + s.shortname + "\n \n" +
               "Открытие: "+ s.open + "\n" +
               "Текущая цена: " + s.marketprice2 + " \n" +
@@ -89,77 +91,80 @@ object MyStockBot extends AuthenticationBot with Polling with InlineQueries with
           }
         }
         if(result==""){
-          reply("Bad request, you probably mistyped secid")
-        }else{
-          reply(result)
+          result = "No info for today"
         }
-
-      }
-    }
-  }
-  onCommand('candles) { implicit msg =>
-    withArgs { args =>
-      val query = args.mkString(" ")
-      val urlJson = Uri("https://iss.moex.com/iss/engines/stock/markets/shares/securities/" + query + "/candles.json")
-        .withQuery(Query("from" -> LocalDate.now.minusDays(7).toString))
-        .toString()
-      for {
-        responseJson <- Http().singleRequest(HttpRequest(uri = Uri(urlJson)))
-        if responseJson.status.isSuccess()
-        json <- Unmarshal(responseJson).to[String]
-      } /* do */ {
-
-        val objs = parse(json)
-        val input = objs \ "candles" \ "data"
-        if(input.isEmpty){
-          reply("Wrong query")
-        }
-        val data = input.extract[List[List[String]]]
-
-        var storage = new ListBuffer[DataCandles]()
-
-        for (d <- data) {
-          storage += DataCandles(d(0), d(1), d(2), d(3), d(4), d(5), d(6), d(7))
-        }
-
-
-        var min = storage(0).close.toFloat
-        var max = storage(0).close.toFloat
-
-//        val dataset = new TimeSeriesCollection()
-//        val series = new TimeSeries("Series")
-
-        val format = new SimpleDateFormat("yyyy-MM-dd")
-        var result = scala.collection.mutable.Map[Date, Float]()
-        for (s <- storage) {
-          result += ( (format.parse(s.end)) -> s.close.toFloat)
-          if (s.close.toFloat < min) {
-            min = s.close.toFloat
-          }
-          if (s.close.toFloat > max) {
-            max = s.close.toFloat
-          }
-        }
-
-
-        val chart = LineChart(result.toCategoryDataset())
-        chart.plot.getRangeAxis.setLowerBound(min)
-        chart.plot.getRangeAxis.setUpperBound(max)
-        chart.title = query
-        val axis = chart.plot.getDomainAxis.asInstanceOf[DateAxis]
-        axis.setDateFormatOverride(new SimpleDateFormat("dd-MMM-yyyy", new Locale("eu", "EU")))
-
-        chart.saveAsPNG("chart.png")
-
-        reply("done")
-
         val photo = InputFile(Paths.get("chart.png"))
         uploadingPhoto // Hint the user
-        request(SendPhoto(msg.source, photo))
+        msg.source
+        request(SendPhoto(msg.source, photo,result))
+      }
+    }
+  }
+  //create graph for last 7 days (Holidays are off days)
+  def createGraph(query:String): Unit ={
+    val urlJson = Uri("https://iss.moex.com/iss/engines/stock/markets/shares/securities/" + query + "/candles.json")
+      .withQuery(Query("from" -> LocalDate.now.minusDays(7).toString))
+      .toString()
+    for {
+      responseJson <- Http().singleRequest(HttpRequest(uri = Uri(urlJson)))
+      if responseJson.status.isSuccess()
+      json <- Unmarshal(responseJson).to[String]
+    } /* do */ {
+
+      val objs = parse(json)
+      val input = objs \ "candles" \ "data"
+      if(input.isEmpty){
+        print("wrong input")
+        return
+      }
+      val data = input.extract[List[List[String]]]
+
+      var storage = new ListBuffer[DataCandles]()
+
+      for (d <- data) {
+        storage += DataCandles(d(0), d(1), d(2), d(3), d(4), d(5), d(6), d(7))
+      }
+
+
+      var min = storage(0).close.toFloat
+      var max = storage(0).close.toFloat
+
+      val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+      var test = List[(Date,Float)]()
+
+      for (s <- storage) {
+        test =  ( (format.parse(s.end)) -> s.close.toFloat) :: test
+        if (s.close.toFloat < min) {
+          min = s.close.toFloat
+        }
+        if (s.close.toFloat > max) {
+          max = s.close.toFloat
+        }
+      }
+
+      val seri = test.toTimeSeries("Price")
+      val chart = XYLineChart(seri)
+      chart.plot.getRangeAxis.setLowerBound(min)
+      chart.plot.getRangeAxis.setUpperBound(max)
+      chart.plot.setBackgroundPaint(Color.WHITE)
+      chart.plot.setDomainGridlinePaint(Color.black)
+      chart.plot.setRangeGridlinePaint(Color.black)
+      chart.plot.setBackgroundAlpha(0.5f)
+      chart.title = query
+      val axis = chart.plot.getDomainAxis.asInstanceOf[DateAxis]
+      axis.setDateFormatOverride(new SimpleDateFormat("dd-MMM-yyyy", new Locale("eu", "EU")))
+
+      chart.saveAsPNG("chart.png")
+  }
+
+  implicit def jdate2jfree(d: Date): RegularTimePeriod = new Second(d)
+  onCommand('candles) { implicit msg =>
+    withArgs { args =>
 
       }
     }
   }
+
 
 
 
@@ -208,7 +213,7 @@ object MyStockBot extends AuthenticationBot with Polling with InlineQueries with
              """.stripMargin)
       } /* or else */ {
         user =>
-          reply(s"${user.firstName}, you must /login first.")
+          reply(s"${user.firstName}, you must /start first.")
       }
   }
 
@@ -324,8 +329,4 @@ object MyStockBot extends AuthenticationBot with Polling with InlineQueries with
     }
   }
 
-  def moexUrl(query: String): String =
-    Uri("https://iss.moex.com/iss/securities.json")
-      .withQuery(Query("q" -> query))
-      .toString()
 }
